074c7e4a37b7e9e7d8d37329d5a1fd65c4a48f85
andoshin11 <shinglish11@gmail.com>
2018-11-07 00:27:13 +0900

    Disable eslint-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,16 +46,6 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-
       if (ctx.isDev) {
         devConfig(config)
         return
