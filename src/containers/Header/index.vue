04f5e4973a97af9a2b94b57ea7fd0ec3d89b8c42
andoshin11 <shinglish11@gmail.com>
2018-11-08 20:21:58 +0900

    Update header styling

--- src/containers/Header/index.vue
+++ src/containers/Header/index.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="Header">
-    <div class="Header__Background" />
-
     <div class="Header__Nav">
       <nav>
         <ul class="Header__NavList">
-          <nuxt-link class="Header__NavItem" exact to="/"><li>Studio Andy</li></nuxt-link>
+          <nuxt-link class="Header__NavItem logo" exact to="/">
+            <li>STUDIO ANDY</li>
+          </nuxt-link>
         </ul>
       </nav>
     </div>
@@ -36,51 +36,38 @@ export default Vue.extend({
 
 <style scoped>
 .Header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
-  height: 300px;
-  position: relative;
-}
-
-.Header::before {
-  content: '';
-  z-index: 10;
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background: radial-gradient(
-    ellipse at center,
-    transparent 0,
-    transparent 36%,
-    rgba(0, 0, 0, 0.65) 100%
-  );
-  opacity: 0.6;
-}
-
-.Header__Background {
-  position: absolute;
-  width: 100vw;
-  height: 300px;
-  overflow: hidden;
-  top: 0;
-  background: url(/header.jpg) center bottom -150px;
-  background-size: cover;
+  height: 130px;
+  margin-bottom: 30px;
 }
 
 .Header__Nav > nav {
-  max-width: 1000px;
+  width: 1180px;
   z-index: 1000;
   margin: 0 auto;
   position: relative;
 }
 
 .Header__NavList {
+  display: flex;
   padding: 16px 0;
 }
 
-.Header__NavItem {
-  color: #fff;
-  cursor: pointer;
+.logo {
+  color: #000;
+  font-weight: bold;
+  letter-spacing: 1px;
+  font-size: 24px;
+}
+
+.logo::after {
+  width: 100%;
+  height: 1px;
+  content: '';
+  display: block;
+  border-bottom: solid 3px #5b3cc4;
 }
 </style>
