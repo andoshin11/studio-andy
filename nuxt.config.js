8b7919307d48245b22ec53bc815e3c9c1b664278
andoshin11 <shinglish11@gmail.com>
2018-11-08 20:21:35 +0900

    Add Google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,17 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        re: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Merriweather'
+      },
+      {
+        re: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=M+PLUS+1p'
+      }
+    ]
   },
 
   /*
