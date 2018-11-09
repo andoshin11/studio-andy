bcc5d9e013c9bae340ee65c6f43555c5760fca94
andoshin11 <shinglish11@gmail.com>
2018-11-09 14:16:26 +0900

    Add presenter for Home container

--- src/containers/Home/presenter.ts
+++ src/containers/Home/presenter.ts
@@ -1,10 +1,29 @@
+import PostEntity from '@/entities/Post'
+import PostRepository from '@/repositories/PostRepository'
+
+export enum ListType {
+  Latest,
+  Popular
+}
+
 export interface IPresenterParams {
+  postRepository: PostRepository
+  listType: ListType
 }
 
 export interface IPresenter {
+  postList: PostEntity[]
 }
 
-export default ({}: IPresenterParams): IPresenter => {
+export default ({ postRepository, listType }: IPresenterParams): IPresenter => {
+  let postList: PostEntity[]
+  if (listType == ListType.Latest) {
+    postList = postRepository.getLatestPosts()
+  } else {
+    throw new Error('Unsupported List Type')
+  }
+
   return {
+    postList
   }
 }
