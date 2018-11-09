42044117a9e20985ad49cb7b9795322294665f70
andoshin11 <shinglish11@gmail.com>
2018-11-09 17:59:15 +0900

    Add Post Date component

---
+++ src/components/Base/Post/Date/Date.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="Date" :class="{ empty: text.length < 1 }">
+    {{ date }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import dayjs from 'dayjs'
+
+interface IData {}
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  data(): IData {
+    return {}
+  },
+  computed: {
+    date(): string {
+      if (this.text.length > 1) {
+        return dayjs(this.text).format('M.D.YY')
+      }
+      return ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+.Date {
+  color: #aaa;
+  font-size: 18px;
+  font-family: MetaSerifPro, serif;
+  margin-bottom: 20px;
+}
+
+.Date.empty {
+  width: 64px;
+  height: 27px;
+  background-color: #eee;
+}
+</style>
