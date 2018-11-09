84fba17ce06e702e2fb68b7ddbad544b9692fbb3
andoshin11 <shinglish11@gmail.com>
2018-11-09 17:59:03 +0900

    Add Post Title component

---
+++ src/components/Base/Post/Title/Title.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1 class="Title" :class="{ empty: title.length < 1 }">
+    {{ title }}
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface IData {}
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  data(): IData {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+.Title {
+  width: 80%;
+  line-height: 1.2;
+  font-size: 40px;
+  font-weight: bold;
+  text-align: center;
+  margin: 0 0 24px;
+}
+
+.Title.empty {
+  height: 48px;
+  background-color: #eee;
+}
+</style>
---
+++ src/components/Base/Post/Title/index.ts
@@ -0,0 +1,3 @@
+import Title from './Title.vue'
+
+export default Title
