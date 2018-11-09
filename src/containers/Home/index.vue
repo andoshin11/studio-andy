334b51ac20f34f62c1b21a6a3ccfc7fd70eb9b1d
andoshin11 <shinglish11@gmail.com>
2018-11-09 14:16:51 +0900

    Use actual data on Home container

--- src/containers/Home/index.vue
+++ src/containers/Home/index.vue
@@ -1,37 +1,68 @@
 <template>
   <div class="Home">
     <div class="PostList">
-      <PostList />
+      <PostList :data="presenter.postList" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import store from '@/store'
 
-import Presenter, { IPresenter } from './presenter'
+import Presenter, { IPresenter, ListType } from './presenter'
 
 // Use Case
+import FetchLatestPostsUseCase from '@/usecases/post/FetchLatestPostsUseCase'
 
 // Repositories
+import PostRepository from '@/repositories/PostRepository'
+
+// Gateway
+import ContentfulGateway from '@/gateway/ContentfulGateway'
 
 // Service
+import ErrorService from '@/services/ErrorService'
 
 // components
 import PostList from '@/components/Modules/PostList'
 
 interface IData {
-  test: boolean
+  listType: ListType
 }
 
 export default Vue.extend({
   components: {
     PostList
   },
+  computed: {
+    presenter(): IPresenter {
+      return Presenter({
+        postRepository: new PostRepository(store),
+        listType: this.listType
+      })
+    }
+  },
   data(): IData {
     return {
-      test: false
+      listType: ListType.Latest
     }
+  },
+  methods: {
+    async fetchLatestPosts() {
+      const usecase = new FetchLatestPostsUseCase({
+        postRepository: new PostRepository(store),
+        errorService: new ErrorService({ context: 'Fetching Latest Posts' }),
+        contentfulGateway: new ContentfulGateway()
+      })
+      await usecase.execute()
+    },
+    showLatest() {
+      this.listType = ListType.Latest
+    }
+  },
+  async mounted() {
+    await this.fetchLatestPosts()
   }
 })
 </script>
