08759a3cb4aed706873de86d8dfce61cf7d9d876
andoshin11 <shinglish11@gmail.com>
2018-11-23 00:59:44 +0900

    Update service worker

---
+++ src/static/main-sw.js
@@ -0,0 +1,119 @@
+workbox.routing.registerRoute(
+  new RegExp('^https://cdn.contentful.com/spaces/2p1otbbee5vt/environments/master/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'entry-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^http://images.ctfassets.net/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^https://i.gyazo.com/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'gyazo-gif-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^https://api-cdn.embed.ly/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'embedly-api-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^http://cdn.embedly.com/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'embedly-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^http://i-cdn.embed.ly/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'embedly-img-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^https://assets-cdn.github.com/favicon.ico'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'github-icon-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14 // for 2 weeks
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^https://fonts.googleapis.com/'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30 // for 1 month
+      })
+    ]
+  })
+)
