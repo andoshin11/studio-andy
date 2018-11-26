e36bcad1eab40f78174b11d847c9de34e4ef14fc
andoshin11 <shinglish11@gmail.com>
2018-11-27 01:09:28 +0900

    Init error view

---
+++ src/layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Error">エラー出ててわろた {{ error }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+})
+</script>
