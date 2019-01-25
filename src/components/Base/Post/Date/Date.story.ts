ec864fa1676f197f5da123bd580fbad3faf593f5
andoshin11 <shinglish11@gmail.com>
2019-01-25 15:08:16 +0900

    Update story

--- src/components/Base/Post/Date/Date.story.ts
+++ src/components/Base/Post/Date/Date.story.ts
@@ -4,7 +4,11 @@ const Date = () => import('./Date.vue')
 storiesOf('components/Base/Post/Date', module)
   .add('Default', () => ({
     components: { Date },
-    template: '<div><Date text="2019/1/25" /></div>'
+    template: '<div><Date text="2019/12/25" /></div>'
+  }))
+  .add('Single Digit', () => ({
+    components: { Date },
+    template: '<div><Date text="2019/1/1" /></div>'
   }))
   .add('Empty', () => ({
     components: { Date },
