00ef636d85b9d0de3008237bfa6de8f52cc4251e
andoshin11 <shinglish11@gmail.com>
2019-01-30 03:10:58 +0900

    Remove Storybook

--- src/components/Base/Loading/Loading.story.ts
+++ src/components/Base/Loading/Loading.story.ts
@@ -1,7 +1,7 @@
-import { storiesOf } from '@storybook/vue'
-const Loading = () => import('./Loading.vue')
+// import { storiesOf } from '@storybook/vue'
+// const Loading = () => import('./Loading.vue')
 
-storiesOf('components/Base/Loading', module).add('Default', () => ({
-  components: { Loading },
-  template: '<div><Loading /></div>'
-}))
+// storiesOf('components/Base/Loading', module).add('Default', () => ({
+//   components: { Loading },
+//   template: '<div><Loading /></div>'
+// }))
