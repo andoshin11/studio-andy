72911ab8aa37cc93b581cc4c378a30936bbe942b
andoshin11 <shinglish11@gmail.com>
2019-02-06 00:10:32 +0900

    Use zisui decorator

---
+++ packages/studio-andy-storybook/.storybook/addons.js
@@ -0,0 +1,1 @@
+import 'zisui/register'
--- packages/studio-andy-storybook/.storybook/config.js
+++ packages/studio-andy-storybook/.storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/vue'
+import { configure, addDecorator } from '@storybook/vue'
+import { withScreenshot } from 'zisui'
+
+addDecorator(withScreenshot())
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.story.tsx?$/)
