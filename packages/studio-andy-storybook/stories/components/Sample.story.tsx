51a3cad43569b5c00c349d75b4f504d199c758c4
andoshin11 <shinglish11@gmail.com>
2019-02-05 22:39:49 +0900

    Update story design

--- packages/studio-andy-storybook/stories/components/Sample.story.tsx
+++ packages/studio-andy-storybook/stories/components/Sample.story.tsx
@@ -1,5 +1,14 @@
 import { storiesOf } from '@storybook/vue'
 
 storiesOf('sample', module).add('default', () => ({
-  template: '<div>sample text</div>'
+  template: `
+  <div class="Storybook">
+    <div class="StorybookSection">
+      <div class="StorybookSection__Header">Sample component</div>
+      <div class="StorybookSection__Body">
+        <div>sample text</div>
+      </div>
+    </div>
+  </div>
+  `
 }))
