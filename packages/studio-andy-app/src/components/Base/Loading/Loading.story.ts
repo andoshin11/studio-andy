742adb5517aee65b32a86f97d731d0352c93ce06
andoshin11 <shinglish11@gmail.com>
2019-08-19 13:05:57 +0900

    Remove storybook

--- packages/studio-andy-app/src/components/Base/Loading/Loading.story.ts
+++
@@ -1,7 +0,0 @@
-// import { storiesOf } from '@storybook/vue'
-// const Loading = () => import('./Loading.vue')
-
-// storiesOf('components/Base/Loading', module).add('Default', () => ({
-//   components: { Loading },
-//   template: '<div><Loading /></div>'
-// }))
--- packages/studio-andy-storybook/test/snapshot/stories.test.js
+++
@@ -1,7 +0,0 @@
-import path from 'path'
-import initStoryshots from '@storybook/addon-storyshots'
-
-initStoryshots({
-  framework: 'vue',
-  configPath: path.join(__dirname, '../../.storybook')
-})
