20c20920fcd85f30f18e64466385c3a8ba98a9cd
andoshin11 <shinglish11@gmail.com>
2020-04-27 01:48:21 +0900

    Deprecate xlink:href

--- src/components/Modules/SocialButtons/SocialButtons.tsx
+++ src/components/Modules/SocialButtons/SocialButtons.tsx
@@ -24,17 +24,17 @@ export default tsx.component({
       <div class={styles.SocialButtons}>
         <a href={`http://b.hatena.ne.jp/entry/${this.pagePath}`} class={styles.socialButton}>
           <svg width="20px" height="40px" viewBox="0 0 141.732 141.732" enable-background="new 0 0 141.732 141.732">
-            <use xlinkHref="/hatena.svg#root" />
+            <use href="/hatena.svg#root" />
           </svg>
         </a>
         <a href={`http://twitter.com/share?url=${this.pagePath}&text=${this.post.props.title}`} class={styles.socialButton} target="_blank">
           <svg height="16" width="20" viewBox="0 0 20 16">
-            <use xlinkHref="/twitter.svg#root" />
+            <use href="/twitter.svg#root" />
           </svg>
         </a>
         <a href={`http://getpocket.com/edit?url=${this.pagePath}`} class={styles.socialButton} target="_blank">
           <svg width="20px" height="40px" viewBox="0 0 141.732 141.732" enable-background="new 0 0 141.732 141.732" xmlSpace="preserve">
-            <use xlinkHref="/pocket.svg#root" />
+            <use href="/pocket.svg#root" />
           </svg>
         </a>
       </div>
