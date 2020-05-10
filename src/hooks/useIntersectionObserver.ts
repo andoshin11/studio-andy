49d724a8c0f018dd9732195c89e5b235b9cd7317
andoshin11 <shinglish11@gmail.com>
2020-05-10 14:31:41 +0900

    Add useIntersectionObserver hook

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,20 @@
+import { Ref, isRef } from 'nuxt-composition-api'
+
+export const useIntersectionObserver = (el: Ref<Element | null> | Element | null, cb: IntersectionObserverCallback) => {
+  const _el = isRef(el) ? el.value : el
+
+  if (!process.client) {
+    throw new Error('IntersectionObserver is only available on client side')
+  }
+
+  if (typeof IntersectionObserver !== 'function') {
+    throw new Error('IntersectionObserver is not registered on window object')
+  }
+
+  if (!_el) {
+    throw new Error('Target element not found')
+  }
+
+  const observer = new IntersectionObserver(cb)
+  observer.observe(_el)
+}
