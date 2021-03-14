e419079dded526703cd3644041cf5f98595a6712
andoshin11 <shinglish11@gmail.com>
2021-03-14 13:32:41 +0900

    Add a type helper

---
+++ src/types/helpers.ts
@@ -0,0 +1,1 @@
+export type UnionToTuple<T> = ((T extends any ? (t: T) => T : never) extends infer U ? ((U extends any ? (u: U) => any : never) extends (v: infer V) => any ? V : never) : never) extends (_: any) => infer W ? [...UnionToTuple<Exclude<T, W>>, W] : []
